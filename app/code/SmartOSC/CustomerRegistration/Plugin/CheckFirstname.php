75c39d5c93900b0393db40abb9765624e479623f
gnocman <namcong.hnue@gmail.com>
2022-12-15 16:19:40 +0700

    HCMT-462 #4

--- app/code/SmartOSC/CustomerRegistration/Plugin/CheckFirstname.php
+++ app/code/SmartOSC/CustomerRegistration/Plugin/CheckFirstname.php
@@ -1,11 +1,19 @@
 <?php
+declare(strict_types=1);
 
 namespace SmartOSC\CustomerRegistration\Plugin;
 
 use Magento\Customer\Controller\Account\CreatePost;
 
+/**
+ * Once a new customer is being registered, the extension checks the First Name field
+ */
 class CheckFirstname
 {
+    /**
+     * @param CreatePost $subject
+     * @return void
+     */
     public function beforeExecute(CreatePost $subject): void
     {
         $firstName = $subject->getRequest()->getParam('firstname');
